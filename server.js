3df630b074760dd91f47aea721aab84d5f97fc88
Emmanuel Ezenwigbo <emmanuelezenwigbo@gmail.com>
2020-07-05 00:46:39 +0100

    Updating Database

--- server.js
+++ server.js
@@ -13,10 +13,8 @@ const image = require('./controllers/image');
 const db = knex({
     client: 'pg',
     connection: {
-      host : 'postgresql-amorphous-45764',
-      user : 'postgres',
-      password : '0000',
-      database : 'facialrecognition'
+      host : process.env.DATABASE_URL,
+      ssl : true
     }
   });
   
